8436d960a8ee2ce2890f0e8e66a977db3e657433
Kevin2606 <andreskevin2606@gmail.com>
2023-08-15 23:18:39 -0500

    feat: :sparkles: Se añade JWT para el acceso a los endpoints

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import { crearToken, validarToken } from './middlewares/middlewareJWT.js'
 import alquilerRouter from './routers/alquilerRouter.js'
 dotenv.config()
 
 const app = express()
 
-app.use(express.json())
-app.use('/token', (req, res) => {
-  res.send('Hola')
-})
-app.use('/', alquilerRouter)
-const config = JSON.parse(process.env.SERVER)
+app.get('/token', crearToken)
+app.use('/', validarToken , alquilerRouter)
 
+const config = JSON.parse(process.env.SERVER)
 app.listen(config, () => {
   console.log(`http://${config.hostname}:${config.port}`)
 })
